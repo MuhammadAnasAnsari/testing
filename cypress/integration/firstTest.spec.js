b2c65d5b485fc589c0f2fe4d7f26bd7895fd3b8e
Anas <anasansarimuhammad@gmail.com>
2022-05-18 18:02:14 +0500

    Web Datepickers

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -2,6 +2,7 @@
 /// <reference types="cypress"/>  
 
 const { Input } = require("@angular/core")
+const { set } = require("core-js/core/dict")
 
 // context() or describe() both are same
 describe("My first test suite", () => {
@@ -373,6 +374,50 @@ describe("My first test suite", () => {
        })
 
     })
+
+    })
+
+    it.only("Web Datepickers", () => {
+        // recursive function declaration  
+        function selectDayfromCurrent(day){
+
+            // dynamic date selection handling
+            let date =new Date()  // variable initiation
+            date.setDate(date.getDate() + day)      // getDate() [will get the current date] + [num] means the date will be [num] days from the current date
+            let futureDay = date.getDate()   // getDay() will return number of Day date i.e 27 
+            //  let futureMonth = date.getMonth()   // getmonth() will return number of month
+            let futureMonth = date.toLocaleString('default', {month: 'short'}) // this line will return the month & its short form i.e Dec
+            let dateAssert = futureMonth+' '+futureDay+', '+date.getFullYear() // to match the input field date format
+            
+            cy.get('nb-calendar-navigation').invoke('attr', 'ng-reflect-date').then( dateAttribute => {
+                
+                if(!dateAttribute.includes(futureMonth)){   // navigate tab short month don't include current month
+                    cy.get('[data-name="chevron-right"]').click() // click on datepicker > button
+                    // cy.get('nb-calendar-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                    selectDayfromCurrent()  // for iteration
+                }else {
+                    // select active date from date picker calender section that contain current day date
+                    cy.get('nb-calendar-picker [class="day-cell ng-star-inserted"]').contains(futureDay).click()
+                }
+            })
+            return dateAssert
+        } 
+
+
+        cy.visit('/')
+        cy.contains('Forms').click()
+        cy.contains('Datepicker').click()
+
+        // To click on Common datepicker input field
+        cy.contains('nb-card', 'Common Datepicker').find('input').then(input =>{
+            cy.wrap(input).click({force:true})  // To click on Common datepicker input field
+
+        let dateAssert = selectDayfromCurrent(5) //Function call        
+              
+
+        cy.wrap(input).invoke('prop', 'value').should('contain', dateAssert)
+
+        })
 
     })
 })
